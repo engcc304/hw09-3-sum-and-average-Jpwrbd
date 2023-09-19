41348056fdd07d56b99d3d72ca67a42572ad1d67
Jpwrbd <jpwrbd456@gmail.com>
2023-09-19 15:36:57 +0700

    up

--- Homework/HW09/hw09-3-sum-and-avg.cpp
+++ Homework/HW09/hw09-3-sum-and-avg.cpp
@@ -1,16 +1,16 @@
 /*
     ผู้ใช้กรอกค่า แล้วบันทึกค่าลงไปในอาเรย์(Array1) จากนั้นให้คุณทำการคัดลอกข้อมูลทั้งหมดลงไปในอาเรย์ตัวใหม่(Array2) และให้คุณทำการแสดงข้อมูลในอาเรย์ตัวใหม่ทั้งหมดบนหน้าจอ (เมื่อผู้ใช้กรอก -1 จะถือเป็นการสิ้นสุดการกรอกข้อมูล)
-    
+
     Test case:
-        Input value to Array1 [0]: 
+        Input value to Array1 [0]:
             9
-        Input value to Array1 [1]: 
+        Input value to Array1 [1]:
             5
-        Input value to Array1 [2]: 
+        Input value to Array1 [2]:
             6
-        Input value to Array1 [3]: 
+        Input value to Array1 [3]:
             8
-        Input value to Array1 [4]: 
+        Input value to Array1 [4]:
             -1
     Output:
         Array1 = 9 5 6 8
@@ -20,23 +20,23 @@
 
 
     Test case:
-        Input value to Array1 [0]: 
+        Input value to Array1 [0]:
             7
-        Input value to Array1 [1]: 
+        Input value to Array1 [1]:
             -3
-        Input value to Array1 [2]: 
+        Input value to Array1 [2]:
             5
-        Input value to Array1 [3]: 
+        Input value to Array1 [3]:
             19
-        Input value to Array1 [4]: 
+        Input value to Array1 [4]:
             27
-        Input value to Array1 [5]: 
+        Input value to Array1 [5]:
             -16
-        Input value to Array1 [6]: 
+        Input value to Array1 [6]:
             13
-        Input value to Array1 [7]: 
+        Input value to Array1 [7]:
             -7
-        Input value to Array1 [8]: 
+        Input value to Array1 [8]:
             -1
     Output:
         Array1 = 7 -3 5 19 27 -16 13 -7
@@ -45,3 +45,43 @@
         Array2 = 7 -3 5 19 27 -16 13 -7
 
 */
+
+#include <stdio.h>
+int main()
+{
+    int array1[100];
+    int count = 0, check;
+    while (check != -1)
+    {
+        printf("Input : \n");
+        scanf("%d", &check);
+        if (check != -1)
+        {
+            array1[count] = check;
+            count++;
+        } // end if
+    }     // end while
+    printf("Array1 =");
+    for (int i = 0; i < count; i++)
+    {
+        printf(" %d", array1[i]);
+    } // end for
+    int array2[count];
+    printf("\nArray2 =");
+    for (int i = 0; i < count; i++)
+    {
+        array2[i] = 0;
+        printf(" %d", array2[i]);
+    } // end for
+    printf("\n--| Copy Data from Array1 to Array2");
+    for (int i = 0; i < count; i++)
+    {
+        array2[i] = array1[i];
+    } // end for
+    printf("\nArray2 =");
+    for (int i = 0; i < count; i++)
+    {
+        printf(" %d", array2[i]);
+    } // end for
+    return 0;
+}
